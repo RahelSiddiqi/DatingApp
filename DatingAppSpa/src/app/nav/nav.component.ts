3a104db99c01cb5bbffdf4abddee163abf6fcb16
Aziz Siddiqi <azizsiddiqirahel@gmail.com>
2018-09-22 11:52:04 +0600

    Component Added

---
+++ DatingAppSpa/src/app/nav/nav.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_service/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {};
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Login Successfull');
+    }, error => {
+      console.log('Login Failed');
+    });
+  }
+
+  loggedIn(){
+    const token = localStorage.getItem('Token');
+    return !!token;
+  }
+
+  logOut(){
+    localStorage.removeItem('Token');
+    console.log('Successfully Logged Out');
+  }
+}
